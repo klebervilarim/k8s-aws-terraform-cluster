64fe87d07e704ab80a40552aa63d2d18d6680961
klebervilarim <klebervilarim@hotmail.com>
2023-07-18 16:28:00 -0300

    Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -6,37 +6,22 @@ variable "AWS_SECRET_KEY" {
 
 }
 
-variable "environment" {
-  default = "staging"
-}
-
 variable "AWS_REGION" {
-  default = "<YOUR_REGION>"
+  default = "us-east-1"
 }
 
 module "k8s-cluster" {
-  ssk_key_pair_name      = "<SSH_KEY_NAME>"
-  uuid                   = "<GENERATE_UUID>"
+  ssk_key_pair_name      = var.ssk_key_pair_name
+  uuid                   = var.uuid
   environment            = var.environment
-  vpc_id                 = "<VPC_ID>"
-  vpc_private_subnets    = "<PRIVATE_SUBNET_LIST>"
-  vpc_public_subnets     = "<PUBLIC_SUBNET_LIST>"
-  vpc_subnet_cidr        = "<SUBNET_CIDR>"
-  PATH_TO_PUBLIC_LB_CERT = "<PAHT_TO_PUBLIC_LB_CERT>"
-  PATH_TO_PUBLIC_LB_KEY  = "<PAHT_TO_PRIVATE_LB_CERT>"
+  vpc_id                 = var.vpc_id
+  vpc_private_subnets    = var.vpc_private_subnets
+  vpc_public_subnets     = var.vpc_public_subnets
+  vpc_subnet_cidr        = var.vpc_subnet_cidr
+  PATH_TO_PUBLIC_LB_CERT = var.PATH_TO_PUBLIC_LB_KEY
+  PATH_TO_PUBLIC_LB_KEY  = var.PATH_TO_PUBLIC_LB_KEY
   install_nginx_ingress  = true
   install_longhorn       = true
-  source                 = "github.com/garutilorenzo/k8s-aws-terraform-cluster"
+  source                 = "github.com/klebervilarim/k8s-aws-terraform-cluster"
 }
 
-output "k8s_dns_name" {
-  value = module.k8s-cluster.k8s_dns_name
-}
-
-output "k8s_server_private_ips" {
-  value = module.k8s-cluster.k8s_server_private_ips
-}
-
-output "k8s_workers_private_ips" {
-  value = module.k8s-cluster.k8s_workers_private_ips
-}
